16dfc7d86b12c51e583f356de23eef069bca3c4a
Ryan William O'Hara <ryan@ryanwohara.com>
2023-11-04 21:47:29 -0600

    refactor: Moving bits into their own fn

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,9 @@ extern crate chrono;
 extern crate reqwest;
 extern crate select;
 
-use anyhow::Result;
 use tokio;
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    match application::run().await {
-        Ok(_) => return Ok(()),
-        Err(e) => {
-            println!("Error: {}", e);
-            return Err(e);
-        }
-    }
+async fn main() {
+    application::run().await
 }
