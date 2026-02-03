c1a53fd4f7142c2fba49c33a901d62ee55e660c2
Ryan William O'Hara <ryan@ryanwohara.com>
2026-02-03 15:48:18 -0700

    fix: Add user-agent header

--- bin/refresh_item_db.sh
+++ bin/refresh_item_db.sh
@@ -3,12 +3,13 @@
 set -ex
 
 dir=$(dirname -- "$0")
+USERAGENT='User-Agent: Reinze.com'
 
 pull() {
   url="$1"
   filename="$2"
 
-  curl "${url}" 2>/dev/null | jq | tee "${dir}/../lib/staging.json" && \
+  curl -H "${USERAGENT}" "${url}" 2>/dev/null | jq | tee "${dir}/../lib/staging.json" && \
       cat "${dir}/../lib/staging.json" | jq && mv "${dir}/../lib/staging.json" "${dir}/../lib/${filename}.json";
 }
 
